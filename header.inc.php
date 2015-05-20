62230fa5330149f4dc96d86435138588928d196a
Larry D Gardner <ldgalaxy@comcast.net>
2015-05-20 15:57:06 -0600

    Using  to extract SCRIPT_NAME from page

---
+++ header.inc.php
@@ -0,0 +1,13 @@
+<?php
+
+$script_name = $_SERVER['SCRIPT_NAME'];
+
+?>
+
+
+
+<form action="<?php echo $script_name; ?>" method="POST">
+
+   <input type="submit" name="submit" value="Submit">
+
+</form>
